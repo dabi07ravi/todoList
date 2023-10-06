ed78fa2cee98a231d74f941f1f91d3d64242bd0d
Arcgate-ravindra <ravindrasingh.dabi@arcgate.com>
2023-10-06 15:30:29 +0530

    my first todo list app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,93 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [inputList, setInputList] = useState("");
+  const [items, setItems] = useState([]);
+  const [edit, setEdit] = useState(null);
+  // Initialize state with data from local storage
+  useEffect(() => {
+    const storedItems = localStorage.getItem("items");
+    if (storedItems) {
+      setItems(JSON.parse(storedItems));
+    }
+  }, []);
+  const handleInputList = (event) => {
+    setInputList(event.target.value);
+  };
+  const handleSubmit = () => {
+    if (edit !== null) {
+      // Edit existing item
+      const updatedItems = [...items];
+      updatedItems[edit] = inputList;
+      setItems(updatedItems);
+      setEdit(null);
+      setInputList("");
+      localStorage.setItem("items", JSON.stringify(updatedItems));
+    } else {
+      // Add new item
+      const updatedItems = [...items, inputList];
+      setItems(updatedItems);
+      setInputList("");
+      localStorage.setItem("items", JSON.stringify(updatedItems));
+    }
+  };
+
+  const handleDel = (index) => {
+    const removedItem = items.splice(index, 1);
+    // Remove deleted item from local storage
+    const updatedItems = [...items];
+    setItems(updatedItems);
+    localStorage.setItem("items", JSON.stringify(updatedItems));
+  };
+
+  const handleEdit = (index) => {
+    const data = items[index];
+    setInputList(data);
+    setEdit(index);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="maindiv">
+        <div className="centerdiv">
+          <h1>Todo List App</h1>
+          <div className="inputdiv">
+            <div className="inputField">
+              <input
+                className="input"
+                type="text"
+                placeholder="add items"
+                onChange={handleInputList}
+                value={inputList}
+              ></input>
+              <button onClick={handleSubmit}>Add</button>
+            </div>
+            <div className="list">
+              <ul>
+                {items.map((item, index) => (
+                  <li className="listItems" key={item}>
+                    {item}
+                    <div className="buttons">
+                      <button
+                        className="deleteButton"
+                        onClick={() => handleDel(index)}
+                      >
+                        delete
+                      </button>
+                      <button
+                        className="editButton"
+                        onClick={() => handleEdit(index)}
+                      >
+                        edit
+                      </button>
+                    </div>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
